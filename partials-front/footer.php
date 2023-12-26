c4e76481bb10ff8ca39d4d03cd2d424e981c043f
Liza Maharjan <leeza.mhr@gmail.com>
2023-12-26 21:26:10 +0545

    feat: all commit

---
+++ partials-front/footer.php
@@ -0,0 +1,79 @@
+<!-- social Section Starts Here -->
+<!-- <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section> -->
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <!-- <section class="footer">
+        <div class="container text-center">
+        </div>
+    </section>
+    footer Section Ends Here -->
+    
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comic Cafe</title>
+
+    
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+
+    <section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>Quick links</h3>
+            <a href="<?php echo SITEURL; ?>index.php"> <i class="fas fa-arrow-right"></i> Home </a>
+            <a href="<?php echo SITEURL; ?>foods.php"> <i class="fas fa-arrow-right"></i> Food </a>
+            <a href="<?php echo SITEURL; ?>categories.php"> <i class="fas fa-arrow-right"></i> Categories </a>
+            <a href="<?php echo SITEURL; ?>contact.php"> <i class="fas fa-arrow-right"></i> Contact </a>
+        </div>
+
+        <div class="box">
+            <h3>Contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +977-980-1112255 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> thecomiccafe.np@gmail.com </a>
+            <a href="#"> <i class="fas fa-envelope"></i> Basantapur, Kathmandu, Nepal </a>
+        </div>
+
+        <div class="box">
+            <h3>Socials</h3>
+            <a href="https://m.facebook.com/Thecomiccafenp-100797172147518/"> <i class="fab fa-facebook-f"></i> Facebook </a>
+            <a href="https://www.instagram.com/thecomiccafe.np/?hl=en"> <i class="fab fa-instagram"></i> Instagram </a>
+            <a href="https://www.linkedin.com/in/rohit-shrestha-6b9819178/?originalSubdomain=np"> <i class="fab fa-linkedin"></i> Linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by Liza Maharjan | all rights reserved </div>
+
+</section>
+
+</body>
+</html>
